d8173fe871e6953b8c2b5158b17c42873799c119
Minh Tâm <102682115+minhtam78945@users.noreply.github.com>
2022-10-26 22:59:29 +0700

    Update

---
+++ index.js
@@ -0,0 +1,58 @@
+
+const handleSilder = () => {
+    const slider = document.querySelector(".slider");
+    const sliderMain = document.querySelector(".slider-main");
+    const sliderNext = document.querySelector(".slider-next");
+    const sliderPrev = document.querySelector(".slider-prev");
+    const sliderItems = document.querySelectorAll(".slider-item");
+    const sliderDotitems = document.querySelectorAll(".slider-dot-item");
+    const sliderItemsWidth = sliderItems[0].offsetWidth;
+    // console.log(sliderItemsWidth);
+    const sliderLength = sliderItems.length;
+
+    let PositionX = 0;
+    let index = 0;
+    const handlechange = (dir) => {
+        if (dir === 1) {
+            if (index >= sliderLength - 1) {
+                index = sliderLength - 1;
+                return;
+            }
+            PositionX -= sliderItemsWidth;
+            sliderMain.style = ` transform : translateX(${PositionX}px)`;
+            index++;
+        }
+        else if (dir === -1) {
+            if (index <= 0) {
+                index = 0;
+                return;
+            }
+            PositionX += sliderItemsWidth;
+            sliderMain.style = ` transform : translateX(${PositionX}px)`;
+            index--;
+        }
+        [...prop].forEach(element => element.classList.remove("active"))
+        sliderDotitems[index].classList.add("active");
+    }
+    sliderNext.addEventListener("click", function () {
+        handlechange(1);
+    });
+    sliderPrev.addEventListener("click", function () {
+        handlechange(-1);
+    });
+    //Toan tu Prop
+    const prop = sliderDotitems;
+    [...prop].forEach(item => (
+
+        item.addEventListener("click", function (e) {
+            [...prop].forEach(element => element.classList.remove("active"))
+            e.target.classList.add("active");
+            const sildeIndex = parseInt(e.target.dataset.index);
+            index = sildeIndex;
+            PositionX = -1 * index * sliderItemsWidth;
+            sliderMain.style = ` transform : translateX(${PositionX}px)`;
+        })
+    ))
+
+};
+window.addEventListener("load", handleSilder)
